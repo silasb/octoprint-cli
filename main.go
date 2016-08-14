a970ba4ea513ecf593989e2f272448a4048e9ae0
silasb <silas.baronda@gmail.com>
2016-08-14 12:49:23 -0400

    commands; upload file

--- main.go
+++ main.go
@@ -2,50 +2,112 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"mime/multipart"
 	"net/http"
 	"os"
 	"path/filepath"
+
+	"github.com/codegangsta/cli"
 )
 
 var host = "http://10.5.5.15:5000/api/"
 var API_KEY = "1234"
 
-func main() {
-	println(API("files"))
+// var (
+// 	uploadCommand = flag.NewFlagSet("upload", flag.ExitOnError)
+// 	questionFlag  = uploadCommand.String("file", "", "Question that you are asking for")
+// )
 
-	// getRequest(API("files"))
+func init() {
 
-	uploadFile("test.gcode")
+	// if len(os.Args) == 1 {
+	// 	fmt.Println("usage: siri <command> [<args>]")
+	// 	fmt.Println("The most commonly used git commands are: ")
+	// 	fmt.Println(" ask   Ask questions")
+	// 	fmt.Println(" send  Send messages to your contacts")
+	// 	os.Exit(2)
+	// }
+
+	// switch os.Args[1] {
+	// case "upload":
+	//     os.Args[2:]
+	// 	uploadCommand.Parse()
+	// default:
+	// 	fmt.Printf("%q is not valid command.\n", os.Args[1])
+	// 	os.Exit(2)
+	// }
+
+	// if uploadCommand.Parsed() {
+	// 	if *questionFlag == "" {
+	// 		fmt.Println("Please supply the question using -question option.")
+	// 		os.Exit(2)
+	// 	}
+	// 	fmt.Printf("You asked: %q\n", *questionFlag)
+	// }
+
+	// username := flag.String("user", "root", "Username for this server")
+	// flag.Parse()
+	// fmt.Printf("Your username is %q.", *username)
+
 }
 
-func uploadFile(path string) {
+func main() {
+	app := cli.NewApp()
+	app.Name = "octoprint"
+	app.Usage = ""
+	app.Commands = []cli.Command{
+		{
+			Name:      "upload",
+			Aliases:   []string{"u"},
+			Usage:     "upload files",
+			ArgsUsage: "[files]",
+			Action: func(c *cli.Context) error {
+				if c.NArg() > 0 {
+					for _, file := range c.Args() {
+						fmt.Print("Uploading file: ", file)
+						status := uploadFile(file)
+						fmt.Println(" =>", status)
+					}
+					return nil
+				}
+
+				return errors.New("missing file")
+			},
+		},
+	}
+
+	app.Run(os.Args)
+}
+
+func uploadFile(path string) string {
 	req, err := assembleRequest(path)
 	if err != nil {
 		log.Panic(err)
 	}
 
-	resBody, err := callClient(req)
+	res, err := callClient(req)
 
 	if err != nil {
 		log.Panic(err)
 	}
 
-	body, _ := ioutil.ReadAll(resBody)
-	println(string(body))
+	// body, _ := ioutil.ReadAll(res.Body)
+
+	return res.Status
+	// println(string(body))
 }
 
-func callClient(req *http.Request) (io.ReadCloser, error) {
+func callClient(req *http.Request) (*http.Response, error) {
 	req.Header.Set("X-API-KEY", API_KEY)
 
 	client := &http.Client{}
 	res, err := client.Do(req)
 
-	return res.Body, err
+	return res, err
 }
 
 func assembleRequest(path string) (*http.Request, error) {
