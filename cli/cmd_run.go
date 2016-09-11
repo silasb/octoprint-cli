54e680b7a24553c03b749c2be0dfc52d4c8a6b7e
silasb <silas.baronda@gmail.com>
2016-09-10 23:32:53 -0400

    Forgot cmd_run.go file

---
+++ cli/cmd_run.go
@@ -0,0 +1,56 @@
+package cli
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/codegangsta/cli"
+	"github.com/silasb/octoprint-cli/api"
+)
+
+func CmdRun() cli.Command {
+	return cli.Command{
+		Name:    "run",
+		Aliases: []string{"r"},
+		Usage:   "run gcode commands",
+		Flags: []cli.Flag{
+			cli.BoolFlag{
+				Name: "verbose, v",
+				//Usage:  "Octoprint host *REQUIRED*",
+			},
+		},
+		//ArgsUsage: "",
+		Action: func(c *cli.Context) error {
+			if c.NArg() > 0 {
+				// handle both stdin and argument based input
+
+				gcode := c.Args().First()
+				commands := strings.Split(gcode, ";")
+				for i, command := range commands {
+					commands[i] = strings.TrimSpace(command)
+				}
+
+				if c.Bool("verbose") {
+					for _, command := range commands {
+						fmt.Printf("%+v\n", command)
+					}
+					return nil
+				}
+
+				err := api.Run(commands)
+				if err != nil {
+					log.Panic(err)
+				}
+				return nil
+			} else {
+				// TODO: interactive mode
+				// -i --interactive
+
+				fmt.Println("TODO: Interactive mode")
+				return nil
+			}
+
+		},
+	}
+}
